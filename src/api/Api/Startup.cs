c611f6e356f7ff88148a8d3a12231c169998062f
Cristiano Cunha <contato@cristianoprogramador.com>
2019-09-05 09:01:02 -0300

    IMplementa a documentação com Swagger

--- src/api/Api/Startup.cs
+++ src/api/Api/Startup.cs
@@ -4,41 +4,51 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 using Repository;
 
 namespace Api
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.InjectRepository();
+            services.InjectBusiness();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Gatel - Documentação", Version = "v0.1"});
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Gatel - Documentação"); });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseCors();
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
     }
-
-    public IConfiguration Configuration { get; }
-
-    public void ConfigureServices(IServiceCollection services)
-    {
-      services.InjectRepository();
-      services.InjectBusiness();
-      services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-    }
-
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    {
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
-      else
-      {
-        app.UseHsts();
-      }
-
-      app.UseCors();
-
-      app.UseHttpsRedirection();
-      app.UseMvc();
-    }
-  }
 }
