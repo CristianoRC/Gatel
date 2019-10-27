dbc54075b26f563e461a47ad34ea51e97b3ea7b8
Cristiano Cunha <contato@cristianoprogramador.com>
2019-10-27 09:28:57 -0300

    Configura o material icon

--- src/app/src/components/user/User.vue
+++ src/app/src/components/user/User.vue
@@ -1,43 +1,47 @@
 <template>
-  <v-container grid-list-xs>
-    <v-card>
-      <v-card-title>
-        Usuários
-        <div class="flex-grow-1"></div>
-        <v-text-field v-model="search" label="Buscar" append-icon="search" single-line hide-details></v-text-field>
-      </v-card-title>
-      <v-data-table
-        :search="search"
-        :headers="headers"
-        :items="users"
-        :items-per-page="10"
-        class="elevation-1"
-      ></v-data-table>
-    </v-card>
-  </v-container>
+	<v-container grid-list-xs>
+		<v-card>
+			<v-card-title>
+				Usuários
+				<div class="flex-grow-1"></div>
+				<v-text-field v-model="search" label="Buscar" append-icon="search" single-line hide-details></v-text-field>
+			</v-card-title>
+			<v-data-table
+				:search="search"
+				:headers="headers"
+				:items="users"
+				:items-per-page="10"
+				class="elevation-1"
+			></v-data-table>
+		</v-card>
+	</v-container>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    search: "",
-    headers: [
-      { text: "#", value: "id" },
-      { text: "Nome", value: "name" },
-      { text: "email", value: "apartament" }
-    ],
-    users: []
-  }),
-  methods: {
-    async getAllUsers() {
-      const responseData = await this.$http.get(
-        "https://localhost:5001/api/user"
-      );
-      return responseData.data;
-    }
-  },
-  async created() {
-    this.users = await this.getAllUsers();
-  }
+	data: () => ({
+		search: "",
+		headers: [
+			{ text: "#", value: "id" },
+			{ text: "Nome", value: "name" },
+			{ text: "email", value: "apartament" }
+		],
+		users: []
+	}),
+	methods: {
+		async getAllUsers() {
+			try {
+				const responseData = await this.$http.get(
+					"https://localhost:5001/api/user"
+				);
+				return responseData.data;
+			} catch (error) {
+				alert("Não foi possível buscar os usuários");
+			}
+		}
+	},
+	async created() {
+		this.users = await this.getAllUsers();
+	}
 };
 </script>
--- src/app/src/main.js
+++ src/app/src/main.js
@@ -4,6 +4,7 @@ import vuetify from './plugins/vuetify';
 import vueRouter from 'vue-router'
 import axios from "axios";
 import router from './router'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
 Vue.use(vueRouter);
@@ -11,8 +12,11 @@ Vue.use(vueRouter);
 // ====== prototype ====== //
 Vue.prototype.$http = axios;
 
+
 new Vue({
   vuetify,
   router,
   render: h => h(App)
 }).$mount('#app')
+
+
