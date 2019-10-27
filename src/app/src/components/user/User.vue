edf6541b546c9ff4d0d2ae5fc6479f156d8c06ff
Cristiano Cunha <contato@cristianoprogramador.com>
2019-10-27 09:52:22 -0300

    Implementa a desativação do usuário

--- src/app/src/components/user/User.vue
+++ src/app/src/components/user/User.vue
@@ -12,32 +12,49 @@
 				:items="users"
 				:items-per-page="10"
 				class="elevation-1"
-			></v-data-table>
+			>
+				<template v-slot:item.action="{ item }">
+					<v-icon small class="mr-2" color="info" @click="editItem(item)">edit</v-icon>
+					<v-icon small color="error" @click="deleteUser(item)">delete</v-icon>
+				</template>
+			</v-data-table>
 		</v-card>
 	</v-container>
 </template>
 
 <script>
+import urls from "../../urls";
+
 export default {
 	data: () => ({
 		search: "",
 		headers: [
 			{ text: "#", value: "id" },
 			{ text: "Nome", value: "name" },
-			{ text: "email", value: "apartament" }
+			{ text: "email", value: "apartament" },
+			{ text: "Actions", value: "action", sortable: false }
 		],
 		users: []
 	}),
 	methods: {
 		async getAllUsers() {
 			try {
-				const responseData = await this.$http.get(
-					"https://localhost:5001/api/user"
-				);
+				const responseData = await this.$http.get(urls.user.getAll);
 				return responseData.data;
 			} catch (error) {
 				alert("Não foi possível buscar os usuários");
 			}
+		},
+		async deleteUser(user) {
+			const confirmMessage = "Você deseja deletar o usuário?";
+			if (confirm(confirmMessage)) {
+				await this.$http.delete(urls.user.delete.replace("@id", user.id));
+				this.removeUserFromArray(user);
+			}
+		},
+		removeUserFromArray(user) {
+			const userIndex = this.users.indexOf(user);
+			this.users.splice(userIndex, 1);
 		}
 	},
 	async created() {
---
+++ src/app/src/urls.js
@@ -0,0 +1,8 @@
+const apiUrl = "https://localhost:5001/api"
+export default {
+  user: {
+    getAll: `${apiUrl}/user`,
+    getById: `${apiUrl}/user/@id`,
+    delete: `${apiUrl}/user/@id`,
+  }
+}
